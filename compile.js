952f184ce996e78188f56e3e07dd853c330fd15e
xebec19 <rohant946@gmail.com>
2024-11-27 21:36:28 +0530

    wip inbox contract

--- compile.js
+++ compile.js
@@ -1,9 +1,41 @@
-// compile code will go here
 const path = require("path");
 const fs = require("fs");
 const solc = require("solc");
 
 const inboxPath = path.resolve(__dirname, "contracts", "Inbox.sol");
-const source = fs.readFileSync(inboxPath);
+const source = fs.readFileSync(inboxPath, "utf8"); // Ensure encoding is correct
 
-module.exports = solc.compile(source, 1);
+// Compile contract
+const input = {
+  language: "Solidity",
+  sources: {
+    "Inbox.sol": {
+      content: source,
+    },
+  },
+  settings: {
+    outputSelection: {
+      "*": {
+        "*": ["abi", "evm.bytecode.object"],
+      },
+    },
+  },
+};
+
+const output = JSON.parse(solc.compile(JSON.stringify(input)));
+
+// Handle compilation errors
+if (output.errors) {
+  output.errors.forEach((err) => {
+    console.error(err.formattedMessage);
+  });
+  throw new Error("Compilation failed");
+}
+
+// Export ABI and Bytecode
+const abi = output.contracts["Inbox.sol"].Inbox.abi;
+const bytecode = output.contracts["Inbox.sol"].Inbox.evm.bytecode.object;
+
+console.log({ abi, bytecode });
+
+module.exports = { abi, bytecode };
--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -1,6 +1,37 @@
-const ganache = require('ganache');
-const { Web3 } = require('web3');
-
+const ganache = require("ganache");
+const { Web3 } = require("web3");
+const assert = require("assert");
 // updated ganache and web3 imports added for convenience
+const { compile } = require("../compile");
+
+const interface = compile.interface,
+  bytecode = compile.bytecode;
 
 // contract test code will go here
+const web3 = new Web3(ganache.provider());
+
+let accounts;
+let inbox;
+
+beforeEach(async () => {
+  // Get a list of all accounts
+  accounts = await web3.eth.getAccounts();
+
+  // Use one of those accounts to deploy
+  // the contract
+  console.log({ compile });
+  inbox = await new web3.eth.Contract(JSON.parse(interface || {}))
+    .deploy({
+      data: compile.bytecode,
+      arguments: ["Hi there!"],
+    })
+    .send({ from: accounts[0], gas: "1000000" });
+});
+
+describe("Inbox", () => {
+  it("deploys a contract", () => {
+    assert(Array.isArray(accounts), "Accounts is an array");
+
+    assert(accounts.length > 2, "There are more than 2 accounts!");
+  });
+});
