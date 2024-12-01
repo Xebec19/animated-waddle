9bd43801243448f21ab45cce07fbed40746d0bcb
xebec19 <rohant946@gmail.com>
2024-12-01 21:36:28 +0530

    fix: updated to solc0.8.19

--- deploy.js
+++ deploy.js
@@ -1,28 +1,20 @@
-const HDWalletProvider = require("@truffle/hdwallet-provider");
-const { Web3 } = require("web3");
-//updated web3 and hdwallet-provider imports added for convenience
-
-// deploy code will go here
+const HDWalletProvider = require("truffle-hdwallet-provider");
+const Web3 = require("web3");
 const { interface, bytecode } = require("./compile");
-const { MNEMONIC, NETWORK_LINK } = require("./environments");
+const { MNEMONIC, NETWORK_URL } = require("./environments.js");
 
-const provider = new HDWalletProvider(MNEMONIC, NETWORK_LINK);
-
+const provider = new HDWalletProvider(MNEMONIC, NETWORK_URL);
 const web3 = new Web3(provider);
 
 const deploy = async () => {
   const accounts = await web3.eth.getAccounts();
 
-  console.log("Attempting to deploy from account", account[0]);
+  console.log("Attempting to deploy from account", accounts[0]);
 
   const result = await new web3.eth.Contract(JSON.parse(interface))
-    .deploy({
-      data: bytecode,
-      arguments: ["Hi there!"],
-    })
+    .deploy({ data: bytecode, arguments: ["Hi there!"] })
     .send({ gas: "1000000", from: accounts[0] });
 
-  console.log("Contract deployed to ", result.options.address);
-  provider.engine.stop();
+  console.log("Contract deployed to", result.options.address);
 };
 deploy();
